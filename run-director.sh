55b04daddc49e6a7e223c4f9bcb5a3893dfb2d12
Bill Havanki <bhavanki@cloudera.com>
2018-02-17 17:39:43 -0500

    Add ASF license

--- run-director.sh
+++ run-director.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+# Copyright 2018 William A. Havanki, Jr.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 CLOUDERA_DIRECTOR_HOME=/home/director
 DIRECTOR_SERVER_HOME="${CLOUDERA_DIRECTOR_HOME}/cloudera-director-server-2.7.0"
 DB_DIR="${CLOUDERA_DIRECTOR_HOME}/db"
